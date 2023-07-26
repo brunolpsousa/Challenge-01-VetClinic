240f761566f25fd4f5fe313f66d2c1fad94c2e03
Bruno Sousa <brunolpsousa@gmail.com>
2023-07-26 12:01:28 -0300

    feat: add get all tutors controller

--- src/controllers/tutor.ts
+++ src/controllers/tutor.ts
@@ -6,3 +6,8 @@ export const createTutor = wrapper(async (req: Request, res: Response) => {
   const tutor = await Tutor.create(req.body)
   res.status(201).json({ tutor })
 })
+
+export const getAllTutors = wrapper(async (_: Request, res: Response) => {
+  const tutors = await Tutor.find({})
+  res.status(200).json({ tutors })
+})
