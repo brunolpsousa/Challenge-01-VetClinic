5f037c36e4b9c25df98ae76ebb5c62957ebc2445
Bruno Sousa <brunolpsousa@gmail.com>
2023-07-26 14:11:12 -0300

    feat: add get pet controller

---
+++ src/controllers/pet.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express'
+import { createCustomError } from '../middleware/errorHandler'
+import Tutor from '../models/Tutor'
+import wrapper from '../middleware/wrapper'
+
+export const getPet = wrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { petId, tutorId } = req.params
+
+    const findPet = await Tutor.findOne(
+      {
+        _id: tutorId,
+        pets: { $elemMatch: { _id: petId } },
+      },
+      { 'pets.$': 1 },
+    )
+
+    if (!findPet) {
+      return next(
+        createCustomError(
+          404,
+          `No pet with id: ${petId} for tutor with id: ${tutorId}`,
+        ),
+      )
+    }
+
+    const pet = findPet.pets[0]
+
+    res.status(200).json({ pet })
+  },
+)
