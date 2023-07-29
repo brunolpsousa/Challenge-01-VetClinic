73b78952d6eacfedd131d48b109af2a98eb7d7c2
Bruno Sousa <brunolpsousa@gmail.com>
2023-07-29 14:39:16 -0300

    refactor(mongo): move URI to a variable

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,7 @@
 import { connect, ConnectOptions } from 'mongoose'
-const { DB_HOST, DB_PORT, DB_NAME } = process.env
 
-export default async () => {
-  return await connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+export default async (url: string) => {
+  return await connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   } as ConnectOptions)
