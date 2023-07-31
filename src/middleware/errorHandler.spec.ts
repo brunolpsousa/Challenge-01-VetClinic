07c930de6102aa825799d52550b94bd1f9b38c6f
Bruno Sousa <brunolpsousa@gmail.com>
2023-07-31 06:30:36 -0300

    tests: add error handler tests

---
+++ src/middleware/errorHandler.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest'
+import { createCustomError } from './errorHandler'
+import errorHandler from './errorHandler'
+import express from 'express'
+import supertest from 'supertest'
+
+const app = express()
+
+app.get('/', () => {
+  throw createCustomError(204, 'test: no content')
+})
+app.use(errorHandler)
+
+describe('Error handler', () => {
+  it('should throw error', () => {
+    const err = () => {
+      throw createCustomError(1, 'test error')
+    }
+    expect(err).toThrowError('test error')
+  })
+
+  it('should return custom error', async () => {
+    await supertest(app).get('/').expect(204)
+  })
+
+  it('shoud have custom message', () => {
+    try {
+      throw createCustomError(2, 'another test')
+    } catch (e) {
+      expect(e.message).toBe('another test')
+    }
+  })
+})
