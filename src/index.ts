5bbac88425f768af933feae3b3d4fc94e3dc7455
Bruno Sousa <brunolpsousa@gmail.com>
2023-08-04 16:03:26 -0300

    refactor: change app.ts to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
 import express from 'express'
+
 import connectDB from '@db/db'
+
 import errorHandler from '@middleware/errorHandler'
+
 import petsRouter from '@routes/pets'
 import tutorsRouter from '@routes/tutors'
-import swaggerUi from 'swagger-ui-express'
-import swaggerDocs from './swagger.json'
+import swaggerRouter from '@routes/swaggerRoute'
+
 require('dotenv').config()
 
 const { DB_PORT, DB_HOST, DB_NAME } = process.env
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 const URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 const app = express()
 app.use(express.json())
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
+app.use(swaggerRouter)
 app.use(petsRouter)
 app.use(tutorsRouter)
 
-app.use(errorHandler)
 app.use((_: express.Request, res: express.Response) =>
   res.status(404).send('Route does not exist'),
 )
+app.use(errorHandler)
 
 const server = async () => {
   try {
